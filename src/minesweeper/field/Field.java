9b1ed6f118fd628a1217fb0fef8cd5cb99e38406
hokruepp <hokruepp@3DLAB14.htwg-konstanz.de>
2014-11-11 12:59:45 +0100

    v0.1

---
+++ src/minesweeper/field/Field.java
@@ -0,0 +1,47 @@
+package minesweeper.field;
+
+public class Field {
+	private Cell[][] field;
+	int x;
+	int y;
+
+	public Field(int width, int height) {
+		field = new Cell[width][height];
+		x = width;
+		y = height;
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				field[i][j] = new Cell(i + 1, j + 1);
+			}
+		}
+	}
+
+	public Cell getCell(int x, int y) {
+		return field[x - 1][y - 1];
+	}
+
+	public void initField(int iNumMines) {
+		for (int i = 0; i < iNumMines; i++) {
+			
+		}
+	}
+	@Override
+	public String toString() {
+		StringBuffer sbLineSep = new StringBuffer();
+		StringBuffer sb = new StringBuffer();
+
+		sbLineSep.append("+");
+		for (int i = 1; i <= x; i++) {
+			sbLineSep.append("---+");
+		}
+		sbLineSep.append("\n");
+
+		for (int i = 1; i <= x; i++) {
+			for (int j = 1; j <= y; j++) {
+				sb.append(sbLineSep).append("| ");
+			}
+		}
+		return sb.toString();
+	}
+}
---
+++ src/minesweeper/tui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author hokruepp
+ *
+ */
+package minesweeper.tui;
